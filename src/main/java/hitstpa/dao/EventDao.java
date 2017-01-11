350bfbe1d3074b1cfaff4c481aeff29f0b546ce5
fieranmason <fieranmason@gmail.com>
2017-01-11 13:19:46 -0800

    Added event artifacts

---
+++ src/main/java/hitstpa/dao/EventDao.java
@@ -0,0 +1,92 @@
+package hitstpa.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.leverage.util.InternalServerException;
+import com.leverage.util.NotFoundException;
+import com.leverage.util.ReferentialIntegrityException;
+
+import hitstpa.model.Event;
+import hitstpa.model.IndividualAction;
+import hitstpa.model.Interaction;
+
+public class EventDao extends AbstractDao <Event>{
+
+	protected EventDao(DataSource dataSource) {
+		super(Event.class, dataSource);
+		
+		rowMapper = new RowMapper<Event>(){
+
+			@Override
+			public Event mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Event event;
+				Integer id;
+				IndividualAction individualAction;
+				Interaction interaction;
+				String name;
+				String description;
+				
+				try
+				{
+					id = rs.getInt("id");
+					name = rs.getString("name");
+					description = rs.getString("description");
+					Integer individualActionId = rs.getInt("individualAction");
+					boolean individualActionIdWasNull = rs.wasNull();
+					Integer interactionId = rs.getInt("interaction");
+					boolean interactionIdWasNull = rs.wasNull();
+					
+					if(individualActionIdWasNull && interactionIdWasNull)
+					{
+						throw new InternalServerException("Event " + id + 
+								" is not associated with either an individual action or an interaction");
+					}
+					else if(!individualActionIdWasNull && !interactionIdWasNull)
+					{
+						throw new InternalServerException("Event " + id + 
+								" is associated with both an individual action and an interaction");
+					}
+					else if(!individualActionIdWasNull)
+					{
+						individualAction = new IndividualActionDao(dataSource).get(individualActionId);
+						event = new Event(id, individualAction, name, description);
+					}
+					else if(!interactionIdWasNull)
+					{
+						interaction = new InteractionDao(dataSource).get(interactionId);
+						event = new Event(id, interaction, name, description);
+					}
+					else
+					{
+						throw new InternalServerException("Illogical data");
+					}
+				}
+				catch(NotFoundException nfe)
+				{
+					logger.error("Database Fault", nfe);
+					event = null;
+				}
+				catch(ReferentialIntegrityException rie)
+				{
+					logger.error("Database Fault", rie);
+					event = null;
+				}
+				catch(InternalServerException ise)
+				{
+					logger.error("Database Fault", ise);
+					event = null;
+				}
+				
+				return event;
+			}
+			
+		};
+		
+	}
+
+}
---
+++ src/main/java/hitstpa/model/Event.java
@@ -0,0 +1,66 @@
+package hitstpa.model;
+
+public class Event {
+
+	private Integer id;
+	private Interaction interaction;
+	private IndividualAction individualAction;
+	private String name;
+	private String description;
+	
+	public Event(Integer id, Interaction interaction, String name, String description)
+	{
+		this.id = id;
+		this.interaction = interaction;
+		this.name = name;
+		this.description = description;
+	}
+	
+	public Event(Integer id, IndividualAction individualAction, String name, String description)
+	{
+		this.id = id;
+		this.individualAction = individualAction;
+		this.name = name;
+		this.description = description;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Interaction getInteraction() {
+		return interaction;
+	}
+
+	public void setInteraction(Interaction interaction) {
+		this.interaction = interaction;
+	}
+
+	public IndividualAction getIndividualAction() {
+		return individualAction;
+	}
+
+	public void setIndividualAction(IndividualAction individualAction) {
+		this.individualAction = individualAction;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
