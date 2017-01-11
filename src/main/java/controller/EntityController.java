812eac6846b62e86268af3a9df24e78a58f9fae2
fieranmason <fieranmason@gmail.com>
2017-01-11 09:17:32 -0800

    created controller package

--- src/main/java/controller/EntityController.java
+++ src/main/java/controller/EntityController.java
@@ -1,4 +1,4 @@
-package hitstpa;
+package controller;
 
 import java.util.List;
 
@@ -14,6 +14,7 @@ import com.leverage.util.InternalServerException;
 import com.leverage.util.NotFoundException;
 import com.leverage.util.ReferentialIntegrityException;
 
+import hitstpa.Application;
 import hitstpa.dao.EntityDao;
 import hitstpa.model.Entity;
 
